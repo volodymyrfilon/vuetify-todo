616c8d13d5ed3539ff285db010b33b1ac384728f
Volodymyr <volodymyrfilon1312@gmail.com>
2024-03-16 15:26:48 +0100

    feat: todo list

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,29 @@
 			</v-list>
 		</v-navigation-drawer>
 
-		<v-app-bar app>
+		<v-app-bar app color="primary" dark src="nature.jpg" prominent>
+			<template v-slot:img="{ props }">
+				<v-img
+					v-bind="props"
+					gradient="to top right, rgba(19,84,122,.5) rgba(128,208,199,.8)"
+				></v-img>
+			</template>
+
 			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-			<v-toolbar-title>Application</v-toolbar-title>
+
+			<v-toolbar-title>Vuetify Todo</v-toolbar-title>
+
+			<v-spacer></v-spacer>
+
+			<v-btn icon>
+				<v-icon>mdi-magnify</v-icon>
+			</v-btn>
+			<v-btn icon>
+				<v-icon>mdi-heart</v-icon>
+			</v-btn>
+			<v-btn icon>
+				<v-icon>mdi-dots-vertical</v-icon>
+			</v-btn>
 		</v-app-bar>
 
 		<v-main>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,59 @@
 <template>
-  <hello-world />
+	<div class="home">
+		<v-list flat class="pt-0">
+			<div v-for="task in tasks" :key="task.id">
+				<v-list-item
+					@click="doneTask(task.id)"
+					:class="{ 'blue lighten-5': task.done }"
+				>
+					<template v-slot:default>
+						<v-list-item-action
+							><v-checkbox :input-value="task.done" color="primary"></v-checkbox
+						></v-list-item-action>
+
+						<v-list-item-content>
+							<v-list-item-title
+								:class="{ 'text-decoration-line-through': task.done }"
+								>{{ task.title }}</v-list-item-title
+							>
+						</v-list-item-content>
+					</template>
+				</v-list-item>
+				<v-divider></v-divider>
+			</div>
+		</v-list>
+	</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+	name: 'Home',
+	data() {
+		return {
+			tasks: [
+				{
+					id: 1,
+					title: 'Wake up',
+					done: false,
+				},
+				{
+					id: 2,
+					title: 'Buy car',
+					done: false,
+				},
+				{
+					id: 3,
+					title: 'Eat bananas',
+					done: false,
+				},
+			],
+		}
+	},
+	methods: {
+		doneTask(id) {
+			let task = this.tasks.filter(task => task.id === id)[0]
+			task.done = !task.done
+		},
+	},
+}
 </script>
