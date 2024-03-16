c90d6dbcc35874110610d197d3d0d8a89acf9698
Volodymyr <volodymyrfilon1312@gmail.com>
2024-03-16 14:45:17 +0100

    feat: about page

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="about pa-6">
+		<h1>About Vuetify Todo</h1>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nemo et
+			accusamus, eveniet cum maxime nulla cumque non repudiandae! Asperiores,
+			quis illo libero sapiente fugiat suscipit deserunt labore facilis
+			voluptatum! Lorem
+		</p>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nemo et
+			accusamus, eveniet cum maxime nulla cumque non repudiandae! Asperiores,
+			quis illo libero sapiente fugiat suscipit deserunt labore facilis
+			voluptatum! Lorem
+		</p>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque nemo et
+			accusamus, eveniet cum maxime nulla cumque non repudiandae! Asperiores,
+			quis illo libero sapiente fugiat suscipit deserunt labore facilis
+			voluptatum! Lorem
+		</p>
+	</div>
+</template>
