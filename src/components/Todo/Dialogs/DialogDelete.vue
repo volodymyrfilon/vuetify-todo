6c20922eeade0590194e2c70c9fa96ac0af090c3
Volodymyr <volodymyrfilon1312@gmail.com>
2024-03-21 18:18:26 +0100

    feat: dialog modal

---
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-dialog :value="true" max-width="400" persistent>
+		<v-card prepend-icon="mdi-map-marker">
+			<v-card-title class="headline">Delete task?</v-card-title>
+			<v-card-text>Are you sure you want to delete this task?</v-card-text>
+
+			<v-card-actions>
+				<v-spacer></v-spacer>
+
+				<v-btn @click="dialog = false" text> No </v-btn>
+
+				<v-btn
+					@click="$store.dispatch('deleteTask', task.id)"
+					text
+					color="red darken-1"
+				>
+					Yes
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+	props: ['task'],
+}
+</script>
+
+<style></style>
--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -16,18 +16,31 @@
 					>
 				</v-list-item-content>
 				<v-list-item-action>
-					<v-btn icon @click.stop="$store.dispatch('deleteTask', task.id)">
+					<v-btn icon @click.stop="dialogs.delete = true">
 						<v-icon color="primary lighten-1">mdi-delete</v-icon>
 					</v-btn>
 				</v-list-item-action>
 			</template>
 		</v-list-item>
 		<v-divider></v-divider>
+
+		<dialog-delete v-if="dialogs.delete" :task="task" />
 	</div>
 </template>
 
 <script>
 export default {
 	props: ['task'],
+	data() {
+		return {
+			dialogs: {
+				delete: false,
+			},
+		}
+	},
+	components: {
+		'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue')
+			.default,
+	},
 }
 </script>
