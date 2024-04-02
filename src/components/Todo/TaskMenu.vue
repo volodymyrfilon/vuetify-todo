dc1727bc399d27a6dd88b0de049caaca84c425cc
Volodymyr <volodymyrfilon1312@gmail.com>
2024-04-02 14:29:50 +0200

    fix: sort&search bug

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -74,7 +74,14 @@ export default {
 				title: 'Sort',
 				icon: 'mdi-drag-horizontal-variant',
 				click() {
-					this.$store.commit('toggleSorting')
+					if (!this.$store.state.search) {
+						this.$store.commit('toggleSorting')
+					} else {
+						this.$store.commit(
+							'showSnackbar',
+							'How dare you trying to sort while searching! :)'
+						)
+					}
 				},
 			},
 		],
